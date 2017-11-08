53f4b0133a3fd77b16e988730f371c5f0f56369d
niuliwei <997696843@qq.com>
2017-11-08 16:54:11 +0800

    dev commit

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("hello xiaoming");
+		System.out.println("hello xiaoming");
 	}
 }
